e6c5b58f0d80c4886ba773722d6a694c5ca45747
jackcoutts <jackwgcoutts@gmail.com>
2022-06-26 21:18:18 +0100

    Genetic algorithm before running in window.

--- pygame_genetic_algo.py
+++ pygame_genetic_algo.py
@@ -26,11 +26,11 @@ def draw_circle(location):
 def first_gen_vectors(step_num, circle_num, start_position):
 
     moves = [10, -10]
-    av = []
+    all_circle_vectors = []
 
     for item in circle_num:
 
-        v = []
+        circle_vectors = []
         x_start = start_position[0]
         y_start = start_position[1]
 
@@ -39,11 +39,126 @@ def first_gen_vectors(step_num, circle_num, start_position):
             x_start += random.choice(moves)
             y_start += random.choice(moves)
 
-            v.append((x_start, y_start))
+            circle_vectors.append((x_start, y_start))
 
-        av.append(v)
+        all_circle_vectors.append(circle_vectors)
 
-    return av
+    return all_circle_vectors
+
+
+def calc_fitness(all_circle_vectors, target_coordinates, window_height):
+
+    distances = []
+
+    for item in all_circle_vectors:
+
+        steps = len(item)
+        destination = item[steps-1]
+        distances.append(math.dist(destination, target_coordinates))
+
+    n_dists = [1 - (item / window_height) for item in distances]  # Normalise (divide by constant & subtract from 1)
+
+    return n_dists
+
+
+def natural_selection(n_dists):
+
+    mating_pool = []
+
+    for index, item in enumerate(n_dists):  # Index represents circle names
+
+        mating_pool.extend([index for i in range(int(item * 100))])  # Occur 100 * normalised distance
+
+    return mating_pool  # Return mating pool array
+
+
+def random_mutation(mutation_rate):  # Output true or false based on input probability
+
+    mut_num = 1/mutation_rate  # Range to guess from
+    num = random.randint(1, mut_num)  # Number to guess
+    guess = random.randint(1, mut_num)  # Guess
+
+    if num == guess:  # If guessed output True
+        return True
+    else:  # Not guessed output False
+        return False
+
+
+def crossing_over(mother, father, vectors, circle_start_pos):  # Mothers list of circles from mating pool
+
+    gene_length = len(vectors[0])
+
+    all_genes = []
+
+    for x, y in zip(mother, father):
+
+        new_gene = [(vectors[x][item], vectors[y][item]) for item in range(gene_length)]
+
+        for index, item in enumerate(new_gene):
+
+            if random_mutation(0.05):
+
+                ax = random.choice(['x','y'])
+                if ax == 'x':
+
+                    mutate = (random.randint(-index-1, index+1)*10) + circle_start_pos[0]
+                    item[0] = mutate
+                else:
+                    mutate = (random.randint(1, index + 1) * 10) + circle_start_pos[1]
+                    item[1] = mutate
+
+            else:
+                pass
+
+        all_genes.append(new_gene)
+
+    return all_genes
+
+
+def reproduction(mating_pool, genes, circle_start_pos):  # Reproduction and crossing over
+
+    mothers = random.sample(mating_pool, len(genes))  # Random sample from mating_pool
+    fathers = random.sample(mating_pool, len(genes))  # Random sample from mating_pool
+
+    new_genes = crossing_over(mothers, fathers, genes, circle_start_pos)
+
+    return new_genes
+
+
+def run_gen_algo(step_num, circle_num, gen_num, start_pos, target_pos, window_height):
+
+    gen_dict = {}
+    gen_0 = first_gen_vectors(step_num, circle_num, start_pos)
+    gen_dict[0] = gen_0
+
+    for item in range(gen_num):
+
+        if item == 0:
+
+            pass
+
+        else:
+
+            genes = gen_dict[item-1]
+            fit = calc_fitness(genes, target_pos, window_height)
+            mp = natural_selection(fit)
+            new_gen = reproduction(mp, genes, start_pos)
+            gen_dict[item] = new_gen
+
+    return gen_dict
+
+
+
+
+
+# Use dictionary to store vecoters for all generations
+
+
+
+
+
+
+
 
 
 
