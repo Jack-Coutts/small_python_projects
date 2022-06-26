32755c74ac03af8d0888c69d84bb8d44f1e8526c
jackcoutts <jackwgcoutts@gmail.com>
2022-06-26 20:10:52 +0100

    Started over. first_gen_vectors function.

--- pygame_genetic_algo.py
+++ pygame_genetic_algo.py
@@ -7,29 +7,49 @@ import random
 import math
 
 
-def create_first_gen(circle_num, step_num, screen_name):  # Create list of circle instances & list of steps per circle
+def draw_rect(location):
 
-    green = (60, 179, 113)  # RGB for green
-    start_pos = (247.5, 550)  # Start position for all circles x is from left and y from top
-    diameter = 5  # Circle diameter
-    movements = [(10, 10), (10, -10), (-10, -10), (-10, 10)]  # List of possible movements
-    circles = [pygame.draw.circle(screen_name, green, start_pos, diameter) for i in range(circle_num)]  # Circles
-    steps = [[random.choice(movements)for i in range(step_num)] for i in range(circle_num)]  # Steps for circles
+    sq_col = (220, 20, 60)  # Crimson red
+    square = pygame.draw.rect(screen, sq_col, pygame.Rect(location, (10, 10)))
 
-    return circles, steps  # List of each circle instance & list of steps for each
+    return square
 
 
-def calc_fitness(square, circles, window_height):  # List of fitness values for each circle
+def draw_circle(location):
 
-    centre_of_square = square.center()  # Coordinates for center of target square
-    # Find the euclidean distance to centre of target square for each circle
-    distances = [math.dist(item, centre_of_square) for item in [circles[i].center() for i in circles]]
-    normalised_dists = [1-(item/window_height) for item in distances]  # Normalise these distances
+    green = (60, 179, 113)
+    circle = pygame.draw.circle(screen, green, (location), 5)
 
-    return normalised_dists  # List of normalised distances where list order denotes circle instance
+    return circle
 
 
+def first_gen_vectors(step_num, circle_num, start_position):
 
+    moves = [10, -10]
+    av = []
+
+    for item in circle_num:
+
+        v = []
+        x_start = start_position[0]
+        y_start = start_position[1]
+
+        for i in step_num:
+
+            x_start += random.choice(moves)
+            y_start += random.choice(moves)
+
+            v.append((x_start, y_start))
+
+        av.append(v)
+
+    return av
+
+
+
+
+
+# Initiate pygame
 pygame.init()
 
 fpsClock = pygame.time.Clock()  # Initiate frames per second
@@ -44,6 +64,10 @@ pygame.display.set_caption('Genetic Algorithm: Circles to Square')
 font_obj = pygame.font.Font('freesansbold.ttf', 16)
 text_surface = font_obj.render('Generation: 0', True, (0, 0, 0))  # Render the text/font
 
+
+vectors = [(20, 20), (40, 40), (60, 60), (80, 80), (90, 90)]
+count = 0
+
 # Run until the user asks to quit
 running = True
 while running:
@@ -57,23 +81,25 @@ while running:
     screen.fill((173, 216, 230))
 
     # Draw solid red square
-    sq_col = (220, 20, 60)  # Crimson red
-    square = pygame.draw.rect(screen, sq_col, pygame.Rect((20, 20), (10, 10)))
+    square = draw_rect((20, 20))
 
-    # Circle start coordinates
-    cx = 247.5
-    cy = 550
 
-    # Draw a solid blue circle in the center
-    circles = [pygame.draw.circle(screen, (60, 179, 113), (cx+random.randint(-40, 40), cy+random.randint(-40, 40)), 5) for i in range(10)]
+    c = draw_circle(vectors[count])
+    count += 1
+
+
 
     # Add the already created text
     screen.blit(text_surface, (450, 50))
+
+
 
     fpsClock.tick(10)  # 10 frames per second
 
     # Flip the display
     pygame.display.flip()  # Update the contents of the entire display
+
+
 
 # Done! Time to quit.
 pygame.quit()
